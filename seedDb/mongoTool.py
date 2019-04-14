2d82ed4e209a6e2a6f61cde3053463dc2770400b
rajat-arora <rajat.arora@hotmail.ca>
2019-04-13 23:54:53 -0400

    merged seed db python files

---
+++ seedDb/mongoTool.py
@@ -0,0 +1,69 @@
+from pymongo import MongoClient
+import os
+import sys
+import multiprocessing
+anagram_dict = {}
+client = ''
+
+def init_words(fileName):
+    words = {}
+    with open(fileName) as f:
+        for line in f:
+            word = line.strip()
+            words[word] = ''
+    return words
+
+
+def init_anagram_dict(words):
+    anagram_dict = {}
+    for word in words:
+        sorted_word = ''.join(sorted(list(word)))
+        if sorted_word not in anagram_dict: 
+            anagram_dict[sorted_word]=[]
+        anagram_dict[sorted_word].append(word)
+    return anagram_dict
+
+
+def find_anagrams(word,anagram_dict):
+    key= ''.join(sorted(list(word)))
+    if key in anagram_dict:
+        return set(anagram_dict[key]).difference(set([word]))
+    return set([])
+
+def anagrams_to_db(skip_n,limit_n):
+    print('Starting process',skip_n//limit_n,'...')
+    print (skip_n, ' ', limit_n)
+    db = client['anagram']
+    anagram_dict_keys = list(anagram_dict.keys())
+    anagrams = db.anagrams
+    for i in range(limit_n):
+        # print(skip_n)
+        if i+skip_n -1 < 0 :
+            key = anagram_dict_keys[i+skip_n]
+        else:
+            key = anagram_dict_keys[i+skip_n -1]
+        anagram = {"key": key,"anagrams":  anagram_dict[key]}
+        anagrams.insert_one(anagram)
+
+
+if __name__ == '__main__':
+    client = MongoClient(os.environ['MONGO_URI'])
+    n_cores = 4              
+    collection_size = 110339 
+    batch_size = round(collection_size/n_cores+0.5)
+    skips = range(0, n_cores*batch_size, batch_size)
+    filname = 'dict.txt'
+    word_dict = init_words(filname)
+    anagram_dict = init_anagram_dict(word_dict.keys())
+    print(len(anagram_dict.keys()))
+
+    processes = [ multiprocessing.Process(target=anagrams_to_db, args=(skip_n,batch_size)) for skip_n in skips]
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+
+
